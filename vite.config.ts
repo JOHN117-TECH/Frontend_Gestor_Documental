4877c3893288f4a2aff0f6fa8ebe1e8eeab6fe87
John <barretodiazjohnalexander@gmail.com>
2024-10-31 12:51:12 -0500

    Se agrego script start y se agrego el puerto en el vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+  },
 })
